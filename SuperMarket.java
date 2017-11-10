d870f7dc6ec7cf603dd05b6d2d8502d27ebde6ab
kratikaswami <swami.kratika@gmail.com>
2017-11-10 14:23:58 -0500

    Create SuperMarket.java

---
+++ SuperMarket.java
@@ -0,0 +1,15 @@
+package ServletPackage;
+
+public class SuperMarket {
+	
+		public String SuperMarketId;
+		public String MarketName;
+		public String Address;
+		public long Phone;
+		public String Email;
+		public String WebPage;
+		
+		
+		
+		
+}
